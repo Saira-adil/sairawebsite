e45ad437aeadc37092368aa858aa711afa77ff8a
Saira-adil <144832432+Saira-adil@users.noreply.github.com>
2024-01-12 03:48:27 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>WELCOME TO LIBRARY MANAGEMENT SYSTEM</title>
+</head>
+<body>
+  <div class="wrapper">
+<header>
+  <div class="logo"> 
+    <img src="Image/bookicon-removebg-preview.png" width="89px" height="89px">
+   </div>
+<nav>
+<ul>
+<li><a href="index.php">HOME</a></li>
+<li><a href="book.php">BOOKS</a></li>
+<li><a href="ADMIN_LOGIN.php">ADMIN_LOGIN</a></li>
+<li><a href="STUDENT_LOGIN.php">STUDENT_LOGIN</a></li>
+</ul>
+  </nav>
+</header>
+<section>
+  <div class="sec_img">
+    <br><br>
+     <div class="box">
+      <br><br>
+      <h1 style="color:white; font-size: 30px; text-align: center;">WELCOME TO LIBRARY MANAGEMENT SYSTEM</h1><br>
+   </div>
+</div>
+</section>
+<footer>
+<p style="color: black;text-align: center;">
+  <br><br>
+  Email:&nbsp Library.@gmail.com
+</p>
+</footer>  
+</div>
+</body>
+</html>
---
+++ studentlogin.php
@@ -0,0 +1,69 @@
+<?php
+include "connection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>STUDENT_LOGIN</title>
+</head>
+<body>
+    <header>
+      <div class="logo"> 
+      <img src="Image/log.jpg" width="150px" height="100px">
+     </div>
+
+<nav>
+  <ul>
+  <li><a href="index.html">HOME</a></li>
+  <li><a href="STUDENT_LOGIN.html">STUDENT_LOGIN</a></li>
+  </ul>
+  </nav>
+    </header>
+    <section>
+        <div class="log_img">
+          <br><br>
+          <div class="box1">
+            <h1 style="text-align: center;font-size: 35px;font-family: Lucida Console;">LIBRARY MANAGEMENT SYSTEM</h1><br>
+            <h1 style="text-align: center;font-size: 25px;">USER LOGIN FORM</h1><br>
+            <form name="login" action="index.html" method="">
+              <br><br>
+              <div class="login">
+               <input class="form-control" stype="text" name="Username" placeholder="Username" required=""><br>
+                <input class="form-control" type="text" name="Password" placeholder="Password" required=""><br><br>
+                <button  onclick="login()" style="background-color:burlywood;width: 300px;height: 40px; ">Login</button>
+            </form>
+            <p style="color: white;padding-left: 15px;">
+              <br><br>
+              <a style="color: white;" href="">Forget Password?</a>&nbsp &nbsp
+              <br><br>
+              New to this website?<a style="color: white;" href="registration.html">Sign Up</a>
+            </p>
+          </div>
+        </div>
+    </section>
+    <?php
+    if(isset($_post['submit']))
+    {
+        $count=0;
+        $res=mysqli_query($db,"select*from'student'where usename='c'&&password='d;");
+       $count=mysqli_num_rows($res);
+       if($count==0)
+       {
+        ?>
+        <script type="home.js"></script>
+            <?php   
+       }  
+    }
+    else{
+        ?>
+        <script type="home.js"></script>
+        <?php
+    }
+?>
+</body>
+</html>
